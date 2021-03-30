12e8b923a1841fc01faced3dcea7afb2e390f03f
Mohammad <mitq8.mt@gmail.com>
2021-03-30 13:24:14 -0700

    update and delete plant server requests is created

--- server/server.js
+++ server/server.js
@@ -35,16 +35,43 @@ app.get("/plants", async(req, res) => {
 //get a plant
 app.get("/plants/:id", async(req, res) => {
   try {
-    const plant = await pool.query("SELECT * FROM plant WHERE plant_id= $1", [req.params.id])
+    const { id } = req.body
+    const plant = await pool.query("SELECT * FROM plant WHERE plant_id= $1", [id])
     res.json(plant.rows)
   } catch (error){
     console.log(error.message);
   }
 })
 //update a plant
+app.put("/plants/:id", async(req, res) => {
+  try {
+    console.log(req.body);
+    const {id} = req.params;
+    const {name, description, image} = req.body;
+
+    const updateNamePlant = await pool.query("UPDATE plant SET name = $1 WHERE plant_id = $2", [name, id]);
+    const updateDescriptionPlant = await pool.query("UPDATE plant SET description = $1 WHERE plant_id = $2", [description, id]);
+    const updateImagePlant = await pool.query("UPDATE plant SET image = $1 WHERE plant_id = $2", [image, id]);
+
+    res.json("plant was updated")
+  } catch (error) {
+    console.log(
+      error.message
+    );
+  }
+})
 
 //delete a plant
+app.delete("/plants/:id", async(req, res) => {
+  try {
+    const { id } = req.body
+    const deletePlant = await pool.query("DELETE FROM plant WHERE plant_id= $1", [id]);
 
+    res.json("plant was deleted")
+  } catch (error){
+    console.log(error.message);
+  }
+})
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
