3f4768fe73edeb59dd14ea23eb7915f06566e682
Mohammad <mitq8.mt@gmail.com>
2021-04-02 00:47:08 -0700

    heroku

--- client/src/components/CreatePlant.js
+++ client/src/components/CreatePlant.js
@@ -34,7 +34,7 @@ const CreatePlant = () => {
         kingdom: plantDetail.kingdom,
         species: plantDetail.species,
       };
-      const response = await axios("/plants", {
+      const response = await axios("http://localhost:5000/plants", {
         method: "POST",
         url: '/',
         headers: {"Content-Type": "application/json"},
