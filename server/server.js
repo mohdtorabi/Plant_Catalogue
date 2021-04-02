96c86f4beb52efbc9fee6fc835d2176b23884d3d
Mohammad <mitq8.mt@gmail.com>
2021-04-02 01:23:13 -0700

    adding .env

--- server/server.js
+++ server/server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app =express();
 const cors = require("cors");
 const pool = require("./db");
-// const path = require('path');
+const path = require('path');
+require('dotenv').config();
 
 
 
 //middleware
 app.use(cors());
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 
 //ROUTES//
@@ -83,9 +84,11 @@ app.delete("/plants/:id", async(req, res) => {
   }
 })
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+console.log(process.env.PORT);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("server has started on port 5000");
