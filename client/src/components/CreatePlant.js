1c48b54aa0a92eed52cb18fb88ab646d5164eac2
Mohammad <mitq8.mt@gmail.com>
2021-04-01 23:53:01 -0700

    deploying heroku

--- client/src/components/CreatePlant.js
+++ client/src/components/CreatePlant.js
@@ -34,7 +34,7 @@ const CreatePlant = () => {
         kingdom: plantDetail.kingdom,
         species: plantDetail.species,
       };
-      const response = await axios("http://localhost:5000/plants", {
+      const response = await axios("http://localhost:5432/plants", {
         method: "POST",
         url: '/',
         headers: {"Content-Type": "application/json"},
--- client/src/components/EditPlant.js
+++ client/src/components/EditPlant.js
@@ -50,7 +50,7 @@ export default function EditPlant({plant}) {
         kingdom: plantDetail.kingdom,
         species: plantDetail.species,
       };
-      const response = await axios(`http://localhost:5000/plants/${plant.plant_id}`, {
+      const response = await axios(`http://localhost:5432/plants/${plant.plant_id}`, {
         method: "PUT",
         url: '/',
         headers: {"Content-Type": "application/json"},
