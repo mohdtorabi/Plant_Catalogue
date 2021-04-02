61e5084b2d89ebac8aa510998111df7531fecc9a
Mohammad <mitq8.mt@gmail.com>
2021-04-02 02:09:50 -0700

    adding .env

--- client/src/components/CreatePlant.js
+++ client/src/components/CreatePlant.js
@@ -34,7 +34,7 @@ const CreatePlant = () => {
         kingdom: plantDetail.kingdom,
         species: plantDetail.species,
       };
-      const response = await axios("/plants", {
+      const response = await axios(`http://localhost:${process.env.PORT}/plants`, {
         method: "POST",
         url: '/',
         headers: {"Content-Type": "application/json"},
